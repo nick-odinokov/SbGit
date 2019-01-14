907efbae860740e66f38ef52b4568919f1f3e3d7
OdiNN <odinokovnn@mail.ru>
2019-01-15 01:02:40 +0300

    Odi in eclipce

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("455544444444444444444");
-        System.out.println("555444444444444444444");
+        System.out.println("odinn in master first");
+
     }
 }
