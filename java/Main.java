d56d136d75e1a1690c9d4feca7dc9a21daff1ece
Kolya <zyamlik@mail.ru>
2019-01-15 01:07:55 +0300

    new branch

--- java/Main.java
+++ java/Main.java
@@ -2,6 +2,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("odinn in master first");
+        System.out.println("odinn in master first");
+        System.out.println("odinn in master first");
+        System.out.println("odinn in master first");
+        System.out.println("odinn in master first");
+        System.out.println("odinn in master first");
+        System.out.println("odinn in master first");
+
+
+        
+
 
     }
 }
