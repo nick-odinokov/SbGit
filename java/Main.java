2155ce2a04ec5ca5c4f14e24de3d8a2cf5d42084
Kolya <zyamlik@mail.ru>
2019-01-15 00:26:40 +0300

    pull req

--- java/Main.java
+++ java/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("testInBranch");
+        System.out.println("testInBranch - test2222222");
+        System.out.println("testInBranch- test2222222");
     }
 }
