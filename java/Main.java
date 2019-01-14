72289144eeb5e68c71a9117783bb5d7e22a612a0
Kolya <zyamlik@mail.ru>
2019-01-15 00:32:42 +0300

    444444444444444

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("testInBranch - test3333333333333");
-        System.out.println("testInBranch- test333333333333");
+        System.out.println("444444444444444444");
+        System.out.println("444444444444444444");
     }
 }
